b3cf00ed70a7f3e7f623bfaa7076bb960411a619
alnye655321 <wiggles@nyedigital.com>
2021-01-30 07:18:29 -0500

    catching axios erros

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -51,50 +51,70 @@ import axios from "axios";
 export default {
   data() {
     return {
-      items: [
-        { header: "Today" },
-        {
-          avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
-          title: "Brunch this weekend?",
-          subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
-        },
-        { divider: true, inset: true },
-        {
-          avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
-          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-          subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
-        },
-        { divider: true, inset: true },
-        {
-          avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
-          title: "Oui oui",
-          subtitle:
-            '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
-          title: "Birthday gift",
-          subtitle:
-            '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: "https://cdn.vuetifyjs.com/images/lists/5.jpg",
-          title: "Recipe to try",
-          subtitle:
-            '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
-        },
-      ],
+      items: [],
     };
   },
   created() {
-    axios.get("http://localhost:8080/api/v1/users").then(function (res) {
-      console.log(res)
-      this.asteroids = res.data.near_earth_objects.slice(0, 10);
-    });
-
-    console.log("test");
+    this.fetchUsers();
+  },
+  methods: {
+    fetchUsers: function () {
+      axios
+        .get("http://localhost:8080/api/v1/users")
+        .then((res) => {
+          let data = res.data;
+          let itemTemps = [];
+          for (let i = 0; i < data.length; i++) {
+            itemTemps[i] = {
+              avatar: "https://cdn.vuetifyjs.com/images/lists/5.jpg",
+              title: data[i].name,
+              subtitle: data[i].email,
+            };
+          }
+          this.items = itemTemps;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
 };
+
+// [
+//   { header: "Today" },
+//   {
+//     avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
+//     title: "Brunch this weekend?",
+//     subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+//   },
+//   { divider: true, inset: true },
+//   {
+//     avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+//     title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+//     subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+//   },
+//   { divider: true, inset: true },
+//   {
+//     avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
+//     title: "Oui oui",
+//     subtitle:
+//       '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+//   },
+//   { divider: true, inset: true },
+//   {
+//     avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
+//     title: "Birthday gift",
+//     subtitle:
+//       '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+//   },
+//   { divider: true, inset: true },
+//   {
+//     avatar: "https://cdn.vuetifyjs.com/images/lists/5.jpg",
+//     title: "Recipe to try",
+//     subtitle:
+//       '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+//   },
+// ],
 </script>
+
+
